122ff3eaad84cc3b569782a98a3a0f5ceca20101
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-04-19 19:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
 // your code here
+	if(num <=1) return num;
+	let fib = [0,1];
+	for(let i = 2;i<=num;i++){
+      fib[i] = fib[i-1]+fib[i-2];
+}
+	return fib[num];
 }
 
 module.exports = fibonacci;
